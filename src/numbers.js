50caf0f6229a94bfe673a7bff4df66be9fd826af
mcongost <mcongost@guellconsulting.com>
2020-11-29 13:03:02 +0100

    Renamed the "formatter" parameter of Number to "format", for better symmetry with Date / DateTime

--- src/numbers.js
+++ src/numbers.js
@@ -1,34 +1,44 @@
 
 import { Field } from "./field";
 
-const FORMATTER = Symbol('FORMATTER');
+const FORMAT = Symbol('format');
 
 export const NUMBER_FORMATTER = new Intl.NumberFormat(navigator.language, {});
 
 class Number extends Field {
 
     constructor(parameters = null) {
-        const {formatter, ...baseParameters} = parameters;
+        const {format, ...baseParameters} = parameters;
         super(baseParameters);
-        this[FORMATTER] = formatter;
-        if (!this[FORMATTER]) {
-            this[FORMATTER] = NUMBER_FORMATTER;
+        this[FORMAT] = format;
+        if (!this[FORMAT]) {
+            this[FORMAT] = NUMBER_FORMATTER;
         }
-        else if (!(this[FORMATTER] instanceof Intl.NumberFormat)) {
-            this[FORMATTER] = new Intl.NumberFormat(navigator.language, this[FORMATTER]);
+        else if (!(this[FORMAT] instanceof Intl.NumberFormat)) {
+            this[FORMAT] = new Intl.NumberFormat(navigator.language, this[FORMAT]);
         }
+    }
+
+    getCopyParameters(options = null) {
+        const parameters = super.getCopyParameters(options);
+        parameters.format = this[FORMAT];
+        return parameters;
     }
 
     static get type() {
         return 'number';
     }
 
-    static get formatter() {
-        return this[FORMATTER];
+    static get format() {
+        return this[FORMAT];
     }
 
     getValueLabel(value, options = null) {
-        const formatter = options && options.formatter || this[FORMATTER];
+        const format = options && options.format || this[FORMAT];
+        let formatter = (
+            format instanceof Intl.NumberFormat ?
+            format : new Intl.NumberFormat(navigator.language, format)
+        );
         return formatter.format(value);
     }
 }
